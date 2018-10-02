8f348bd61dba0f542952d91d8ff1a0c94d9a518f
Jasmine Walters <jsmnwltrs@gmail.com>
2018-10-02 18:59:26 -0500

    close button works

--- javascript/components/details.js
+++ javascript/components/details.js
@@ -1,8 +1,17 @@
 import {printToDom} from "../helpers/util.js"
+import { charactersBuilder } from "./characters.js";
+
+const closeButton = () => {
+    const closeButton = document.getElementById('close');
+    closeButton.addEventListener("click", charactersBuilder)
+};
 
 const detailsBuilder = (character) => {
 let domString = 
     `<div class="col-6 offset-md-3">
+        <div class="row">
+            <button class="btn btn-danger" id="close">X</button>
+        </div>
         <div class="row">
             <div class="col">
                 <img src="${character.imageUrl}" alt="${character.name}"/>
@@ -13,8 +22,8 @@ let domString =
             </div>
         </div>
     </div>`;
-
     printToDom(domString);
+    closeButton();
 };
 
 export {detailsBuilder};
